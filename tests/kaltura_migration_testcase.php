448c0ed5804d9052417b65372407089f7f24fac0
Esteve Badia <badia.orive@gmail.com>
2022-06-21 19:05:33 +0200

    Unit test for regexp function

---
+++ tests/kaltura_migration_testcase.php
@@ -0,0 +1,30 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once(__DIR__ . '/fixtures/testable_controller.php');
+
+class kaltura_migration_testcase extends advanced_testcase {
+   public function test_extract_urls() {
+      $text = 'XXX aaa <a href="https://tube.switch.ch/video/1234567890?embed=true">video link</a>' . "\n"
+         . 'http://cast.switch.ch/casts/123 http://cast.switch.com/casts/123';
+      $migration = new testable_kaltura_migration_controller();
+      $urls = $migration->extractUrls($text);
+      $this->assertEquals(count($urls), 2);
+      $this->assertEquals($urls[0], 'https://tube.switch.ch/video/1234567890?embed=true');
+      $this->assertEquals($urls[1], 'http://cast.switch.ch/casts/123');
+   }
+}
